4e2fe60ab202ee88faa40d9b091dae530172fc70
Inoland <contact@nobandegan.com>
2025-05-25 10:15:04 +0400

    Update app.js

--- static/app.js
+++ static/app.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   await populateAudioDevices();
 
   const proto = location.protocol === 'https:' ? 'wss:' : 'ws:';
-  ws = new WebSocket(`${proto}://${window.location.host}/ws`);
+  ws = new WebSocket(`ws://${window.location.host}/ws`);
 
   ws.onopen = () => {
     console.log("WebSocket connected, requesting saved config...");
