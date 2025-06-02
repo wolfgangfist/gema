058b47b91037961bf769f931b324a04c00ce37c7
Inoland <contact@nobandegan.com>
2025-06-02 12:29:20 +0400

    Update windows-install.bat

--- windows-install.bat
+++ windows-install.bat
@@ -15,22 +15,20 @@ call conda create --name csm_streaming_api python=3.10 pip=25.0
 
 call conda activate csm_streaming_api
 
-
-echo Installing dependencies...
-pip install -r requirements.txt
-
-
-call conda deactivate
-
-call conda activate csm_streaming_api
-
 call conda install conda-forge::cuda-runtime=12.8.1 conda-forge::cudnn=9.8.0.87
 
 call conda deactivate
 
 call conda activate csm_streaming_api
 
-pip install sounddevice
+echo Installing dependencies...
+pip install -r requirements.txt
+
+call conda deactivate
+
+call conda activate csm_streaming_api
+
+call huggingface-cli login
 
 echo.
 echo Install complete!
